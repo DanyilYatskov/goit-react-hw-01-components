ff5965f36213cf8111cf540a2021af375e70dfc6
DanyilYatskov <68867876+DanyilYatskov@users.noreply.github.com>
2021-04-01 00:31:28 +0300

    addded friends proptypes

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import FriendListItem from './FriendListItem';
 import styles from './friendList.module.scss';
 
@@ -8,10 +9,26 @@ const FriendList = ({ friends }) => {
       <h2 className={styles.title}>List of Friends</h2>
       <ul className={styles.friendList}>
         {friends.map(friend => (
-          <FriendListItem key={friend.id} friend={friend} />
+          <FriendListItem
+            key={friend.id}
+            avatar={friend.avatar}
+            name={friend.name}
+            isOnline={friend.isOnline}
+          />
         ))}
       </ul>
     </div>
   );
 };
 export default FriendList;
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    }),
+  ).isRequired,
+};
--- src/components/FriendList/FriendListItem.js
+++ src/components/FriendList/FriendListItem.js
@@ -1,19 +1,22 @@
 import React from 'react';
+//import PropTypes from 'prop-types';
 import styles from './friendList.module.scss';
-const FriendListItem = ({ friend }) => {
+const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
-    <li key={friend.id} className={styles.item}>
+    <li className={styles.item}>
       <span
         className={styles.status}
         style={
-          friend.isOnline
-            ? { backgroundColor: 'green' }
-            : { backgroundColor: 'red' }
+          isOnline ? { backgroundColor: 'green' } : { backgroundColor: 'red' }
         }
       ></span>
-      <img className={styles.avatar} src={friend.avatar} alt="" width="48" />
-      <p className={styles.name}>{friend.name}</p>
+      <img className={styles.avatar} src={avatar} alt="" width="48" />
+      <p className={styles.name}>{name}</p>
     </li>
   );
 };
 export default FriendListItem;
+FriendListItem.defaultProps = {
+  avatar:
+    'https://www.edmundsgovtech.com/wp-content/uploads/2020/01/default-picture_0_0.png',
+};
